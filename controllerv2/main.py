dd442458586951b2e2745911675a7491a8c3255c
richard <jiang.j.qian@qq.com>
2020-01-03 17:14:16 +0800

    last commit of sd-wan-controller

---
+++ controllerv2/main.py
@@ -0,0 +1,64 @@
+from controllerv2.core import Singleton
+
+from flask import render_template
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+@app.route("/index.html")
+@app.route("/index.htm")
+def index():
+    region = "Baicells"
+    fatedges = [
+        {
+            'Name': 'Beijing',
+            'SN': 'Unknown',
+            'IP': 'Unknown',
+            'NetworkModel': 'Unknown',
+            'Online': 'OFF',
+            'SPEC': 'Unknown',
+            'SW': 'Unknown',
+            'ThinEdges': [
+                {
+                    'Name': 'Nanjing',
+                    'SN': 'Unknown',
+                    'IP': 'Unknown',
+                    'Online': 'OFF',
+                    'SPEC': 'Unknown',
+                    'SW': 'Unknown',
+                    'Tunnels': 'Unknown'
+                },
+                {
+                    'Name': 'Shenzhen',
+                    'SN': 'Unknown',
+                    'IP': 'Unknown',
+                    'Online': 'OFF',
+                    'SPEC': 'Unknown',
+                    'SW': 'Unknown',
+                    'Tunnels': 'Unknown'
+                },
+                {
+                    'Name': 'AliCloud',
+                    'SN': 'Unknown',
+                    'IP': 'Unknown',
+                    'Online': 'OFF',
+                    'SPEC': 'Unknown',
+                    'SW': 'Unknown',
+                    'Tunnels': 'Unknown'
+                },
+            ]
+        },
+    ]
+    return render_template("overview.html", region=region, fatedges=fatedges)
+
+@app.route("/todo")
+@app.route("/todo/")
+def todo():
+    return render_template("todo.html")
+
+
+if __name__ == "__main__":
+    app.run()
+
